80af46c0a6d8d69cff6de6821163f34f3c5595eb
Emily Morton <emilyrosemorton@gmail.com>
2025-08-14 20:38:56 +0100

    fixed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import Image from "next/image";
+// import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 
@@ -27,7 +27,7 @@ export default function Home() {
       >
         <main className={styles.main}>
           <h1>First Change</h1>
-          <Image alt="" />
+          {/* <Image alt="" /> */}
         </main>
       </div>
     </>
